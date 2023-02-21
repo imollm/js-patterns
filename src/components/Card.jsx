29d5e2aaf387ddda04efc3b13feb01a49c667c63
Ivan Moll <ivanmoll07@gmail.com>
2023-02-21 10:09:11 +0100

    Add function to change title

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,24 @@
-// import { Link } from 'react-router'
-
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Card ({ name }) {
+  const linkRef = useRef('')
   const patternName = name.split(' ')[0]
 
+  const changeTitle = () => {
+    const title = document.querySelector('head title')
+    title.innerText = ''
+    title.innerText = `JS Patterns - ${linkRef.current.dataset.pattern}`
+  }
+
   return (
-    <div className='border-4 bg-darkGray text-lightYellow border-[#BABABA] rounded flex items-center justify-center w-[650px] h-64 my-0 mx-auto hover:text-darkGray hover:bg-darkYellow hover:scale-110 hover:animate-active'>
-      <Link to={`/pattern/${patternName.toLowerCase()}`} className='w-full h-full flex justify-center items-center'>
+    <div onClick={changeTitle} className='border-4 bg-darkGray text-lightYellow border-[#BABABA] rounded flex items-center justify-center w-[650px] h-64 my-0 mx-auto hover:text-darkGray hover:bg-darkYellow hover:scale-110 hover:animate-active'>
+      <Link
+        to={`/pattern/${patternName.toLowerCase()}`}
+        className='w-full h-full flex justify-center items-center'
+        data-pattern={patternName}
+        ref={linkRef}
+      >
         <div className='text-2xl flex justify-center'>
           <span>{name}</span>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,12 @@ import { JSIcon } from './Icons'
 import { Link } from 'react-router-dom'
 
 export default function Header () {
+  const changeTitle = () => {
+    const title = document.querySelector('head title')
+    title.innerText = ''
+    title.innerText = 'JS Patterns'
+  }
+
   return (
     <header className='bg-darkGray w-full h-20 px-32 flex justify-between'>
       <div className='flex gap-8 items-center'>
@@ -11,7 +17,10 @@ export default function Header () {
         <span className='text-lightGray'>JavaScript Patterns</span>
       </div>
       <div className='flex items-center text-lightGray'>
-        <Link to='/'>
+        <Link
+          to='/'
+          onClick={changeTitle}
+        >
           Home
         </Link>
       </div>
